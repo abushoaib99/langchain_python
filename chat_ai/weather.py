203ff218c6d3bde7843e09bbee67062bea09606f
abushoaib99 <abushoaib99@gmail.com>
2026-06-05 13:16:23 +0600

    update python version requirement and add weather agent functionality

---
+++ chat_ai/weather.py
@@ -0,0 +1,20 @@
+# pip install -qU langchain "langchain[google-genai]"
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="google_genai:gemini-2.5-flash-lite",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What's the weather in dhaka today?"}]}
+)
+print(result["messages"][-1].content_blocks)
